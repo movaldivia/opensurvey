95ac924984f670d5338d88ca70ce9136a2984d6c
movaldivia <movaldivia@uc.cl>
2023-09-15 16:08:37 -0300

    feat: add forms dynamically

--- app/forms/page.tsx
+++ app/forms/page.tsx
@@ -1,9 +1,37 @@
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+type Question = {
+  type: "text";
+};
+
+const generateQuestion = (): Question => {
+  return {
+    type: "text",
+  };
+};
+
+type RenderQuestionProps = {
+  text: string;
+};
+
+const renderQuestion = (props: Question) => {
+  if (props.type === "text") {
+    return <div>hola</div>;
+  }
+  return null;
+};
 
 export default function Forms() {
+  const questions = [generateQuestion()];
   return (
     <div className="my-24 mx-24">
-      <div>Create form</div>
+      <div>
+        <Button>Add Question</Button>
+      </div>
+      {questions.map((element) => {
+        return renderQuestion(element);
+      })}
       <div className="my-12">
         <Input
           className="border-0 shadow-none focus-visible:ring-0 !focus:border-0 !active:border-0 text-lg font-semibold p-4"
