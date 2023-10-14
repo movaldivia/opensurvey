b44958558d667feb6ae7347f17d671fbdc4b666d
movaldivia <movaldivia@uc.cl>
2023-10-14 14:29:35 -0300

    feat: add faq

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { checkIfUserIsLoggedIn } from "@/lib/actions/actions";
 import { RegisterDialog } from "./registerDialog";
-import { GitHubLogoIcon, ArrowRightIcon } from "@radix-ui/react-icons";
-import { Badge } from "@/components/ui/badge";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { SiteHeader } from "@/components/site-header";
+import { SiteFooter } from "@/components/site-footer";
 import Image from "next/image";
 
 export default async function Home() {
@@ -59,6 +59,82 @@ export default async function Home() {
           />
         </div>
       </section>
+      <section className="px-8 md:px-48 mt-20 mb-20">
+        <div className="md:px-48">
+          <div>
+            <h3 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+              It's time to move on from Google Forms.
+            </h3>
+            <p className="leading-8 text-lg [&:not(:first-child)]:mt-6 text-slate-500 font-normal">
+              Google Forms has been widely used for many years. We believe it's
+              time for an alternative, which is why we created Opensurvey.
+            </p>
+          </div>
+          <div className="mt-12">
+            <h3 className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0">
+              How does Google Forms manage our data?
+            </h3>
+            <p className="leading-8 text-lg [&:not(:first-child)]:mt-6 text-slate-500 font-normal">
+              They can make any claim or promise, but without access to the
+              codebase, how can we truly verify? That's why we launched
+              OpenSurvey as an open-source alternative.
+            </p>
+          </div>
+          <div className="mt-12">
+            <h3 className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0">
+              Tired of Google Forms?
+            </h3>
+            <p className="leading-8 text-lg [&:not(:first-child)]:mt-6 text-slate-500 font-normal">
+              You're not alone! Approximately 80% of the forms I've filled out
+              have been Google Forms. That's why we decided to create an
+              open-source alternative with fresh designs for both us and our
+              users. We're committed to regularly adding new features,
+              prioritizing the ones most requested by our community.
+            </p>
+          </div>
+          <div className="mt-12">
+            <h3 className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0">
+              Do you want to own your form data?
+            </h3>
+            <p className="leading-8 text-lg [&:not(:first-child)]:mt-6 text-slate-500 font-normal">
+              When you use Google Forms, your data is stored on their servers.
+              But what if you want to keep that data on your local machine? With
+              OpenSurvey, you can host your own surveys and have complete
+              control over your data.
+            </p>
+          </div>
+          <div className="mt-12">
+            <h3 className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0">
+              Don't have money?
+            </h3>
+            <p className="leading-8 text-lg [&:not(:first-child)]:mt-6 text-slate-500 font-normal">
+              One of the most compelling reasons people use Google Forms is
+              because it's free. With OpenSurvey, you can host it on your own
+              using Vercel (Next.js) and Supabase (PostgreSQL) for free, just
+              like we did.
+            </p>
+          </div>
+          <div className="mt-12">
+            <h3 className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight transition-colors first:mt-0">
+              OpenSurvey currently offers the following features:
+            </h3>
+            <p className="leading-4 text-lg [&:not(:first-child)]:mt-6 text-slate-500 font-normal">
+              1. Quick form creation in under 1 minute.
+            </p>
+            <p className="leading-4 text-lg [&:not(:first-child)]:mt-6 text-slate-500 font-normal">
+              2. Easy publication and viewing of responses with charts.
+            </p>
+            <p className="leading-4 text-lg [&:not(:first-child)]:mt-6 text-slate-500 font-normal">
+              3. Data export to Excel files.
+            </p>
+            <p className="leading-4 text-lg [&:not(:first-child)]:mt-6 text-slate-500 font-normal">
+              4. Support for short answer, multiple-choice, and single-choice
+              questions."
+            </p>
+          </div>
+        </div>
+      </section>
+      <SiteFooter />
     </div>
   );
 }
---
+++ components/site-footer.tsx
@@ -0,0 +1,31 @@
+import { siteConfig } from "@/config/site";
+
+export function SiteFooter() {
+  return (
+    <footer className="py-6 md:px-40 md:py-0 mt-32">
+      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+        <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
+          Built with ❤️ by{" "}
+          <a
+            href={"https://github.com/movaldivia"}
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            movaldivia
+          </a>{" "}
+          The source code is available on{" "}
+          <a
+            href={siteConfig.links.github}
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            GitHub
+          </a>
+          .
+        </p>
+      </div>
+    </footer>
+  );
+}
